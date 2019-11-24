cf4a6e28daf91672b112eb5e8ee9949242764119
isntlazy <isntlazy@gmail.com>
2019-11-24 15:57:12 +0200

    jwt and password hashing with bcrypt

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,4 +1,13 @@
 const { User } = require('../models')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+
+function jwtSignUser (user) {
+  const ONE_WEEK = 60 * 60 * 24 * 7
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  })
+}
 
 module.exports = {
   async register (req, res) {
@@ -8,6 +17,36 @@ module.exports = {
     } catch (e) {
       res.status(400).send({
         error: 'Ваш email уже зарєстровано в системі'
+      })
+    }
+  },
+  async login (req, res) {
+    try {
+      const { email, password } = req.body
+      const user = await User.findOne({
+        where: {
+          email
+        }
+      })
+      if (!user) {
+        res.status(403).send({
+          error: 'Некоректні дані входу'
+        })
+      }
+      const isPasswordValid = user.comparePassword(password)
+      if (!isPasswordValid) {
+        res.status(403).send({
+          error: 'Некоректні дані входу'
+        })
+      }
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
+    } catch (e) {
+      res.status(500).send({
+        error: 'Виникла проблема входу в систему'
       })
     }
   }
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,9 +1,31 @@
-module.exports = (sequelize, DataTypes) => sequelize.define('User', {
-  email: {
-    type: DataTypes.STRING,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING
+const bcrypt = require('bcrypt')
+
+function hashPassword (user, options) {
+  if (!user.changed('password')) {
+    return
   }
-})
+  const hash = bcrypt.hashSync(user.password)
+  user.setDataValue('password', hash)
+}
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING
+    }
+  }, {
+    hooks: {
+      beforeCreate: hashPassword,
+      beforeUpdate: hashPassword,
+      beforeSave: hashPassword
+    }
+  })
+  User.prototype.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password)
+  }
+  return User
+}
